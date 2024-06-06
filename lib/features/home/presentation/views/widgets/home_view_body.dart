0153633cd65c12fc62a441ae07f3bd5031a4b8b3
sayed <engsayed1993@gmail.com>
2024-06-07 01:23:24 +0300

    best seller item part3

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utilits/assets.dart';
 import 'package:bookly_app/core/utilits/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -25,7 +26,9 @@ class HomeViewBody extends StatelessWidget {
             "Best Seller",
             style: Styles.textStyle18,
           ),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           BestSellerListViewItem(),
         ],
       ),
@@ -33,42 +36,4 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-        aspectRatio: 2.5/4,
-        child: Container(
-          
-          decoration:  BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.red,
-              image: const DecorationImage(
-                fit: BoxFit.fill,
-            image: AssetImage(AssetsData.testImage),
-          )),
-        ),
-      ),
-      const SizedBox(width: 30,),
-       Column(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .5,
-            child: const Text('Harry potter and the Goblet of fire',
-            style: Styles.textStyle20,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            ))
-        ],
-      )
-        ],
-      ),
-    );
-  }
-}
