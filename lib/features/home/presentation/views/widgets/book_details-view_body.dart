d87528beb39e7916b142d915b792affe39059a12
sayed <engsayed1993@gmail.com>
2024-06-10 18:32:07 +0300

    create box action

--- lib/features/home/presentation/views/widgets/book_details-view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details-view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/core/utilits/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_appbar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -37,9 +39,14 @@ class BookDetailsViewBody extends StatelessWidget {
                   fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
             ),
           ),
-          BookRating(mainAxisAlignment: MainAxisAlignment.center,)
+         const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+         const SizedBox(height: 37,),
+         const BooksAction(),
         ],
       ),
     );
   }
 }
+
